96c7e76e3c5985522236a6ea9430ae9829470782
JaquePaula <jaquemarques.santos@gmail.com>
2025-04-12 20:13:29 -0300

    feat: enabled entities and mapped them to database tables

--- Culturapp/Models/EventLocation.cs
+++ Culturapp/Models/EventLocation.cs
@@ -1,11 +1,19 @@
-// namespace Culturapp.Models
-// {
-//   public class EventLocation
-//   {
-//     [Key]
-//     public int Id { get; set; }
-//     public string? Name { get; set; }
-//     public string? Telephone { get; set; }
-//     public int? Capacity { get; set; }
-//   }
-// }
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Culturapp.Models
+{
+  [Table("EventLocations")]
+  public class EventLocation
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+    // [ForeignKey("AddressId")]
+    // public Address Address { get; set; }
+    public string? Telephone { get; set; }
+    public int? Capacity { get; set; }
+  }
+}
