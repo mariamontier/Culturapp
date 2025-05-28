cfe0cb44f7f74c17dd4c2f2bfa48f990c6d689c4
mafhemontier <mariafernandamontier20@outlook.com>
2025-05-28 02:07:53 -0300

    feat: create and style Home component; update Signup component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
@@ -8,6 +9,8 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'eventpage', component: EventpageComponent },
+  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) }
+
   // { path: '**', redirectTo: 'login' },
 ];
 
