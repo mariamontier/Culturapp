6eb758aa7dd2a58c52dfb97feb79e2704ab52902
Igor Tudisco <igortudisco@hotmail.com>
2025-05-29 18:26:28 -0300

    fix: rename CheckInDate to CheckingDate in Checkings table and update model snapshot

---
+++ Culturapp/Migrations/20250529212527_CheckingDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Culturapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckingDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CheckInDate",
+                table: "Checkings",
+                newName: "CheckingDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CheckingDate",
+                table: "Checkings",
+                newName: "CheckInDate");
+        }
+    }
+}
