a9a9ba27021ee8806ee4781b1a4cc9606c1faef7
Igor Tudisco <igortudisco@hotmail.com>
2025-06-01 22:08:51 -0300

    fix: rename UserFullName property to FullName in RegisterRequest model for consistency

--- Culturapp/Models/Requests/RegisterRequest.cs
+++ Culturapp/Models/Requests/RegisterRequest.cs
@@ -6,7 +6,7 @@ namespace Culturapp.Models.Requests
   public class RegisterRequest
   {
     public string? UserName { get; set; }
-    public string? UserFullName { get; set; }
+    public string? FullName { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
     public AccountType AccountType { get; set; }
