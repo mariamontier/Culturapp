a97017d3999bf9cef65c24fb257933a6c50e7277
JaquePaula <jaquemarques.santos@gmail.com>
2025-06-04 19:25:53 -0300

    fix: adjust phone number validation to accept 8 or 9 digits

--- Culturapp/Models/Requests/PhoneRequest.cs
+++ Culturapp/Models/Requests/PhoneRequest.cs
@@ -7,9 +7,9 @@ namespace Culturapp.Models.Requests
     public string? CountryCode { get; set; }
     public string? AreaCode { get; set; }
 
-    // [StringLength(11, MinimumLength = 11)]
+    // [StringLength(9, MinimumLength = 8)]
     // [RegularExpression(@"^\(\d{2}\)\s?\d{4,5}-\d{4}$", ErrorMessage = "Formato de telefone inválido. Ex: (11) 98765-4321")]
-    [StringLength(11, MinimumLength = 11)]
+    [StringLength(9, MinimumLength = 8)]
     public string? PhoneNumber { get; set; }
 
   }
