d6d643af37fcae2ed518a95563812cb24aaa0c79
Igor Tudisco <igortudisco@hotmail.com>
2025-06-04 21:45:14 -0300

    feat: implement client and enterprise user services with API integration

--- frontend/src/app/services/client-user.service.ts
+++ frontend/src/app/services/client-user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { ClientUserResponse } from '../models/client-user-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientUserService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5115/api/ClientUser';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getClientUsers(): Observable<ClientUserResponse[]> {
+    return this.http.get<ClientUserResponse[]>(`${this.apiUrl}/GetClientUsers`);
+  }
+
+  getClientUserById(id: number): Observable<ClientUserResponse> {
+    return this.http.get<ClientUserResponse>(`${this.apiUrl}/GetClientUserById/${id}`);
+  }
 }
--- frontend/src/app/services/enterprise-user.service.ts
+++ frontend/src/app/services/enterprise-user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { EnterpriseUserResponse } from '../models/enterprise-user-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnterpriseUserService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5115/api/EnterpriseUser';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getEnterpriseUsers(): Observable<EnterpriseUserResponse[]> {
+    return this.http.get<EnterpriseUserResponse[]>(`${this.apiUrl}/GetEnterpriseUsers`);
+  }
+
+  getEnterpriseUserById(id: number): Observable<EnterpriseUserResponse> {
+    return this.http.get<EnterpriseUserResponse>(`${this.apiUrl}/GetEnterpriseUserById/${id}`);
+  }
 }
