3076aec59d4178b178fdc7fa4816fe1944130db1
Igor Tudisco <igortudisco@hotmail.com>
2025-06-04 21:30:47 -0300

    fix: remove commented-out validation for phone number format

--- Culturapp/Models/Requests/PhoneRequest.cs
+++ Culturapp/Models/Requests/PhoneRequest.cs
@@ -9,10 +9,6 @@ namespace Culturapp.Models.Requests
 
     // [StringLength(9, MinimumLength = 8)]
     // [RegularExpression(@"^\(\d{2}\)\s?\d{4,5}-\d{4}$", ErrorMessage = "Formato de telefone inválido. Ex: (11) 98765-4321")]
-<<<<<<< HEAD
-=======
-
->>>>>>> 0c3e50169e4a853a480eb44ef8829acf07f14c46
     [StringLength(9, MinimumLength = 8)]
 
     public string? PhoneNumber { get; set; }
