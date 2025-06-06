583ea4c25e749ab14dfdee9c6aafd39f626a5309
Igor Tudisco <igortudisco@hotmail.com>
2025-06-06 15:31:33 -0300

    feat: implement AuthGuard for route protection and add unit tests

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,16 +9,17 @@ import { FaqComponent } from './pages/faq/faq.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { PerfilUsuarioComponent } from './pages/perfil-usuario/perfil-usuario.component';
 import { PerfilEmpresaComponent } from './pages/perfil-empresa/perfil-empresa.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
 
+  // rota inicial
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 
+  // Rotas públicas
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'eventpage', component: EventpageComponent },
-  { path: 'criar-evento', component: CriarEventoComponent },
-  { path: 'editar-evento', component: EditarEventoComponent },
   { path: 'faq', component: FaqComponent },
   { path: 'sobre', component: SobreComponent },
   {
@@ -26,9 +27,14 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
-  { path: 'perfil-usuario', component: PerfilUsuarioComponent },
-  { path: 'perfil-empresa', component: PerfilEmpresaComponent },
 
+  // Rotas protegidas
+  { path: 'criar-evento', component: CriarEventoComponent, canActivate: [AuthGuard] },
+  { path: 'editar-evento', component: EditarEventoComponent, canActivate: [AuthGuard] },
+  { path: 'perfil-usuario', component: PerfilUsuarioComponent, canActivate: [AuthGuard] },
+  { path: 'perfil-empresa', component: PerfilEmpresaComponent, canActivate: [AuthGuard] },
+
+  // Rota curinga
   { path: '**', redirectTo: 'home' }
 ];
 
