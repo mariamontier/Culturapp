877c9d8cea32eb18d763d38d8984122f07ee7d51
Igor Tudisco <igortudisco@hotmail.com>
2025-06-07 21:43:20 -0300

    fix: update EnterpriseUserResponse model to ensure Id is non-nullable and reorder properties

--- Culturapp/Models/Responses/EnterpriseUserResponse.cs
+++ Culturapp/Models/Responses/EnterpriseUserResponse.cs
@@ -2,14 +2,12 @@ namespace Culturapp.Models.Responses
 {
   public class EnterpriseUserResponse
   {
-    public int? Id { get; set; }
-    public string? CNPJ { get; set; }
+    public int Id { get; set; }
     public string? UserName { get; set; } // this is the fantasy name of the enterprise
     public string? FullName { get; set; } // this is the name of the enterprise
+    public string? CNPJ { get; set; }
     public string? Email { get; set; }
-
-    public int? FAQId { get; set; }
-
+    public ICollection<EventResponse?>? Events { get; set; }
     public ICollection<PhoneResponse?>? Phones { get; set; }
     public int? AddressId { get; set; }
     public AddressResponse? Address { get; set; }
